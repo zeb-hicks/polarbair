606ef3df883b3ee237abff5dc05340aa7fd78ec3
Zeb Hicks <Nimphious@gmail.com>
2017-11-25 09:49:19 +1100

    Added ignores for raw assets folder for now. Also added base Entity class and a Bear class that inherits it.

---
+++ PolarbAir/Bear.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PolarbAir {
+    public class Bear : Entity {
+        public Bear(Game game) : base(game) {
+
+        }
+    }
+}
---
+++ PolarbAir/Entity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace PolarbAir {
+    public class Entity : DrawableGameComponent {
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public float Rotation { get; set; }
+
+        public List<Entity> Children = new List<Entity>();
+
+        public Entity(Game game) : base(game) {
+            
+        }
+
+        public Entity(Entity parent) : base(parent.Game) {
+            
+        }
+
+        ~Entity() {
+            foreach (var child in Children) {
+                child.Dispose();
+            }
+        }
+    }
+}
